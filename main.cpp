d320b628351801ea335d3f5d21b3544e3c29d99f
Sara Feychting <sara.feychting@gmail.com>
2015-10-28 14:21:18 +0100

    a lot of shit

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include "main.h"
 
 #include <iostream>
+#include <cmath>
+#include <stdlib>
 using namespace std;
 
 int main() {
@@ -12,13 +14,13 @@ int main() {
     int n;
     cin >> n; //number of elements
 
-    int x[n];
-    int y[n];
+    long x[n];
+    long y[n];
 
     int i = 0;
-    int temp;
+    long temp;
 
-    while(i<n) {
+    while (i < n) {
         cin >> temp;
         x[i] = temp;
         cin >> temp;
@@ -26,6 +28,75 @@ int main() {
         i++;
     }
 
-    cout << x[n-1];
+    //cout << x[n-1];
+    //cout << y[n-1];
 
+    int matrix[n][n];
+    long distance;
+    long xdist;
+    long ydist;
+    long sqrtval;
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            xdist = x[i] - x[j];
+            ydist = y[i] - y[j];
+            //cout << pow(xdist, 2);
+            sqrtval = (pow(xdist, 2) + pow(ydist, 2));
+            distance = sqrt(sqrtval);
+            matrix[i][j] = round(distance);
+            matrix[j][i] = round(distance);
+        }
+    }
+
+    int hash[n];
+    int totlegth = 0;
+    i = 0;
+    j = 1;
+    while (j < n - 1) {
+        hash[i] = j;
+        totlegth += matrix[i][j];
+        i = j + 1;
+        hash[j] = i;
+        totlegth += matrix[j][i];
+        j = i + 1;
+    }
+    hash[n - 1] = 0;
+    totlegth += matrix[n - 1][0];
+
+    // TODO calc totlegth
+
+    int r1;
+    int r2;
+    int swap;
+    int newdist;
+    int go = 0;
+    int fu = 100000;
+    while (go<fu) {
+        go++;
+        r1 = rand() % n;
+        r2 = rand() % n;
+        if (r1 = r2) continue;
+        if (hash[r1] = r2) continue;
+        swap = hash[r1];
+        newdist = matrix[r2][swap] + matrix[swap][hash[r2]] + matrix[r1][hash[swap]]
+              - matrix[r1][swap] - matrix[r2][hash[r2]];
+        if (newdist < 0) {
+        hash[r1] = hash[swap];
+        hash[swap] = hash[r2];
+        hash[r2] = swap;
+    }
+        totlegth += newdist;
+    }
+    order[n];
+    int counter = 0;
+    int stop = hash[0];
+    order[0] = counter;
+    while(stop!=0) {
+        counter++;
+        order[stop]=counter;
+        stop = hash[stop];
+    }
+
+
+    //cout << matrix[n-1][n-2];
 };
